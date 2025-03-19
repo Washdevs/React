7c21e1177863ab4788e963e0526c411f62ff9c0d
Washington <lepexgamer102000@gmail.com>
2025-03-18 22:40:53 -0300

    feat: Entender o Projeto e  Estrutura da Aplicação

---
+++ 02-estrutura-da-aplicacao/src/App.jsx
@@ -0,0 +1,10 @@
+import { Post } from "./Post";
+import "./App.css";
+
+export function App() {
+  return (
+    <div>
+      <Post autor="Diego" desc="Programador" />
+    </div>
+  );
+}
---
+++ 02-estrutura-da-aplicacao/src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+  return (
+    <div>
+      <h1>{props.autor}</h1>
+      <h1>{props.desc}</h1>
+    </div>
+  );
+}
