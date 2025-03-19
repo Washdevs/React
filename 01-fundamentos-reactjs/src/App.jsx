69a0e141f96a831e9a44aaa3ba24c40761f9fab2
Washington <lepexgamer102000@gmail.com>
2025-03-18 22:05:00 -0300

    feat: testando e implementando Componentes

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,14 @@
 //Arquivo react .jsx é o arquivo que é componente. Um componente é o arquivo que contém uma função que retoran um HTML
 // JSX = JavaScript + XML
 
-import { Post, Button, Mensagem } from "./Post"; //Da biblioteca nós importamos o nome do método que vamos usar
+import { Post } from "./Post"; //Da biblioteca nós importamos o nome do método que vamos usar
 
 export function App() {
   //Para listar vários componentes, é necessário colocar todos dentro de uma div
   return (
     <div>
-      <Post />
-      <Mensagem />
-      <Button />
+      <Post author="Diego" content="Opa, Tarde, Bão?" />
+      <Post author="Carlos" content="Uai, Bão Demais Uai!" />
     </div>
   );
 }
--- 01-fundamentos-reactjs/src/Post.jsx
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -1,5 +1,15 @@
-export function Post() {
-  return <h1>Opa</h1>;
+import { useState } from "react";
+import "./App.css";
+
+export function Post(props) {
+  return (
+    <div className="componentePost">
+      <strong>
+        <p>{props.author}</p>
+      </strong>
+      <h2>{props.content}</h2>
+    </div>
+  );
 } //Namer export, apenas a função
 
 // export default Post; = Default, arquivo inteiro
@@ -14,4 +24,24 @@ export function Button() {
 
 export function Mensagem() {
   return <p>Clique No botão abaixo para enviar uma mensagem</p>;
+}
+
+export function Capslock(props) {
+  const textoCapslock = props.mensagem.toUpperCase();
+  return <div>{textoCapslock}</div>;
+}
+
+export function Contador() {
+  const [count, setCount] = useState(0); // use state da o valor inicial e a variavel é o primeiro valor do array e o segundo é a função responsável por atualiza o valor da variável
+
+  function incrementador() {
+    setCount(count + 1); //Pode ser desta forma ou ((prevCount) => prevCount + 1)
+  }
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementador}>Adicionar</button>
+    </div>
+  );
 }
