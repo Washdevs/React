483789d2dc72fb8c81c8e5a9be66bd9dc2ca5665
Washington <lepexgamer102000@gmail.com>
2025-03-20 23:17:12 -0300

    feat: adicionando posto para conter 2 campos post no arquivo

--- 02-estrutura-da-aplicacao/src/App.jsx
+++ 02-estrutura-da-aplicacao/src/App.jsx
@@ -1,8 +1,8 @@
-import { Header } from "../components/Header";
-import { Post } from "./Post";
 import styles from "./App.module.css";
 import "./global.css";
+import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import { Post } from "../components/Post";
 
 export function App() {
   return (
@@ -11,7 +11,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post autor="Marcos" desc="Suporte" />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
