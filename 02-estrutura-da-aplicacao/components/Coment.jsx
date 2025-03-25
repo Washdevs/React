45d89b78153f7024b3b65d940197c8382de43951
Washington <lepexgamer102000@gmail.com>
2025-03-24 22:57:43 -0300

    feat: Formulário de comentários adicionado

---
+++ 02-estrutura-da-aplicacao/components/Coment.jsx
@@ -0,0 +1,34 @@
+import styles from "./Coment.module.css";
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import { Avatar } from "./Avatar";
+
+export function Coment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar src='https://github.com/deyvin.png' />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>deyvin</strong>
+              <time title="20 de março" dateTime="2025-03-20 22:25" className={styles.data}> Cerca de 1h atrás</time>{" "}
+            </div>
+
+            <button title="Deletar">
+              <Trash size={22} />
+            </button>
+          </header>
+
+          <p>Muito bom Parabens</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
