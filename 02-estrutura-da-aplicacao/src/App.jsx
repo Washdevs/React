d33ad696f58274cad3040bbfd53b1ac7445a1358
Washington <lepexgamer102000@gmail.com>
2025-03-26 22:52:59 -0300

    feat: Iterando no projeto

--- 02-estrutura-da-aplicacao/src/App.jsx
+++ 02-estrutura-da-aplicacao/src/App.jsx
@@ -4,6 +4,42 @@ import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Post } from "../components/Post";
 
+// author: {avatar_url: '', name: '', role: ''}
+// content: String
+// publishedAt: Date
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Washdevs.png',
+      name: 'Washington',
+      role: 'Suporte de TI'
+    },
+    content: [
+      { type: 'paragraph', content: 'Iai Pessoal 👋' },
+      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
+      { type: 'paragraph', content:  'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content:  '👉 jane.design/doctorcare'}
+    ],
+    publishedAt: new Date(Date.now() - 60 * 60 * 1000)
+  },{
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/cannyster.png',
+      name: 'Jhonanthan',
+      role: 'Desenvolvedor Full Stack'
+    },
+    content: [
+      { type: 'paragraph', content: 'Iai Pessoal 👋' },
+      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
+      { type: 'paragraph', content:  'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 👉'},
+      { type: 'link', content:  'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date(Date.now() - 120 * 60 * 1000)
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -11,8 +47,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          { posts.map(post => {
+            return (
+              <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+              />)
+          } ) }
         </main>
       </div>
     </div>
