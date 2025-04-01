79bc8089410611a7d82a8f955f773641d5134bc6
Washington <lepexgamer102000@gmail.com>
2025-03-31 21:27:05 -0300

    feat: IDENTANDO

--- 02-estrutura-da-aplicacao/components/Coment.jsx
+++ 02-estrutura-da-aplicacao/components/Coment.jsx
@@ -1,34 +1,37 @@
-import styles from "./Coment.module.css";
-import { ThumbsUp, Trash } from "@phosphor-icons/react";
-import { Avatar } from "./Avatar";
+import styles from './Coment.module.css';
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
 
-export function Coment({content}) {
-  return (
-    <div className={styles.comment}>
-      <Avatar hasBorder={false} src='https://github.com/deyvin.png' />
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <strong>deyvin</strong>
-              <time title="20 de março" dateTime="2025-03-20 22:25" className={styles.data}> Cerca de 1h atrás</time>{" "}
-            </div>
-
-            <button title="Deletar">
-              <Trash size={22} />
-            </button>
-          </header>
-
-          <p>{content}</p>
-        </div>
-
-        <footer>
-          <button>
-            <ThumbsUp />
-            Aplaudir <span>20</span>
-          </button>
-        </footer>
+export function Coment({ content }) {
+ return (
+  <div className={styles.comment}>
+   <Avatar hasBorder={false} src="https://github.com/deyvin.png" />
+   <div className={styles.commentBox}>
+    <div className={styles.commentContent}>
+     <header>
+      <div className={styles.authorAndTime}>
+       <strong>deyvin</strong>
+       <time title="20 de março" dateTime="2025-03-20 22:25" className={styles.data}>
+        {' '}
+        Cerca de 1h atrás
+       </time>{' '}
       </div>
+
+      <button title="Deletar">
+       <Trash size={22} />
+      </button>
+     </header>
+
+     <p>{content}</p>
     </div>
-  );
+
+    <footer>
+     <button>
+      <ThumbsUp />
+      Aplaudir <span>20</span>
+     </button>
+    </footer>
+   </div>
+  </div>
+ );
 }
--- 02-estrutura-da-aplicacao/src/App.jsx
+++ 02-estrutura-da-aplicacao/src/App.jsx
@@ -1,63 +1,58 @@
-import styles from "./App.module.css";
-import "./global.css";
-import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
-import { Post } from "../components/Post";
+import styles from './App.module.css';
+import './global.css';
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+import { Post } from '../components/Post';
 
 // author: {avatar_url: '', name: '', role: ''}
 // content: String
 // publishedAt: Date
 
 const posts = [
-  {
-    id: 1,
-    author: {
-      avatarUrl: 'https://github.com/Washdevs.png',
-      name: 'Washington',
-      role: 'Suporte de TI'
-    },
-    content: [
-      { type: 'paragraph', content: 'Iai Pessoal 👋' },
-      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
-      { type: 'paragraph', content:  'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'link', content:  '👉 jane.design/doctorcare'}
-    ],
-    publishedAt: new Date(Date.now() - 60 * 60 * 1000)
-  },{
-    id: 2,
-    author: {
-      avatarUrl: 'https://github.com/cannyster.png',
-      name: 'Jhonanthan',
-      role: 'Desenvolvedor Full Stack'
-    },
-    content: [
-      { type: 'paragraph', content: 'Iai Pessoal 👋' },
-      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
-      { type: 'paragraph', content:  'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 👉'},
-      { type: 'link', content:  'jane.design/doctorcare'}
-    ],
-    publishedAt: new Date(Date.now() - 120 * 60 * 1000)
-  }
-]
+ {
+  id: 1,
+  author: {
+   avatarUrl: 'https://github.com/Washdevs.png',
+   name: 'Washington',
+   role: 'Suporte de TI',
+  },
+  content: [
+   { type: 'paragraph', content: 'Iai Pessoal 👋' },
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz' },
+   { type: 'paragraph', content: 'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+   { type: 'link', content: '👉 jane.design/doctorcare' },
+  ],
+  publishedAt: new Date(Date.now() - 60 * 60 * 1000),
+ },
+ {
+  id: 2,
+  author: {
+   avatarUrl: 'https://github.com/cannyster.png',
+   name: 'Jhonanthan',
+   role: 'Desenvolvedor Full Stack',
+  },
+  content: [
+   { type: 'paragraph', content: 'Iai Pessoal 👋' },
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz' },
+   { type: 'paragraph', content: 'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 👉' },
+   { type: 'link', content: 'jane.design/doctorcare' },
+  ],
+  publishedAt: new Date(Date.now() - 120 * 60 * 1000),
+ },
+];
 
 export function App() {
-  return (
-    <div>
-      <Header />
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <main>
-          { posts.map(post => {
-            return (
-              <Post
-              key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-              />)
-          } ) }
-        </main>
-      </div>
-    </div>
-  );
+ return (
+  <div>
+   <Header />
+   <div className={styles.wrapper}>
+    <Sidebar />
+    <main>
+     {posts.map(post => {
+      return <Post key={post.id} author={post.author} content={post.content} publishedAt={post.publishedAt} />;
+     })}
+    </main>
+   </div>
+  </div>
+ );
 }
