12759bc47ee2e2af7e2e44874bde63cb29bca260
Washington <lepexgamer102000@gmail.com>
2025-03-31 21:26:35 -0300

    feat: definindo a linha como unique key do conteúdo da linha 63 a 68. useState para alterar e adicionar comentário aos eventos onChange e onSubmit.

--- 02-estrutura-da-aplicacao/components/Post.jsx
+++ 02-estrutura-da-aplicacao/components/Post.jsx
@@ -1,58 +1,94 @@
-import styles from "./Post.module.css";
-import { Coment } from "./Coment";
-import { Avatar } from "./Avatar";
-import{ format, formatDistanceToNow} from 'date-fns';
-import {ptBR} from 'date-fns/locale/pt-BR'
-import { Paragraph } from "@phosphor-icons/react";
+import styles from './Post.module.css';
+import { Coment } from './Coment';
+import { Avatar } from './Avatar';
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
-export function Post({author, publishedAt, content }) {
+//Estado = variaveis que quero que o componente monitore
 
-const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
+export function Post({ author, publishedAt, content }) {
+ const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
   locale: ptBR,
-})
-const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+ });
+
+ const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
   locale: ptBR,
-  addSuffix: true
-})
+  addSuffix: true,
+ });
 
-return (
-    <article className={styles.post}>
-      <header className={styles.head}>
-        <div className={styles.author}>
-          <Avatar src={author.avatarUrl} />
-          <div className={styles.authorInfo}>
-            <strong>{author.name}</strong>
-            <span>{author.role}</span>
-          </div>
-        </div>
-        <time title= {publishedDateFormatted } dateTime={publishedAt.toISOString()} className={styles.data}>
-          {publishedDateRelativeNow}
-        </time>
-      </header>
+ const [comments, setComments] = useState(['Top']);
+ // Temos um array de comentários que começa com o valor inicial "Top".
 
-      <div className={styles.content}>
-      {content.map(line => {
-        if(line.type === 'paragraph'){
-                  return <p>{line.content}</p>
-        }if(line.type === 'link'){
-          return <p><a href="#">{line.content}</a></p>
-        }
-        else{
-          return <p>Erro ao retornar Valores</p>
-        }
-        })}
-      </div>
-      <form className={styles.commentForm}>
-        <strong>Deixe seu Feedback</strong>
-        <textarea placeholder="Deixe seu Feedback aqui" />
-        <footer><button type="submit">Comentar</button></footer>
-      </form>
+ const [newCommentText, setNewCommentText] = useState('');
+ // O texto do novo comentário começa como uma string vazia.
 
-      <div className={styles.commentList}>
-        <Coment />
-        <Coment />
-        <Coment />
-      </div>
-    </article>
-  );
+ function handlleCreateNewCommentChange(event) {
+  setNewCommentText(event.target.value);
+
+  // Pega o valor digitado no input e atualiza o estado com ele.
+ }
+
+ function handlleCreateNewComment(event) {
+  event.preventDefault();
+  // Impede o comportamento padrão do HTML ao enviar o formulário (recarregar a página).
+
+  setComments([...comments, newCommentText]); // Adicionar um novo comentário com o valor. Nesta ordem.
+  /* Usa o spread operator (...) para copiar todos os comentários atuais.
+       Depois, adiciona o novo comentário (newCommentText) ao final desse novo array.
+       Por fim, chama setComments() para atualizar o estado com esse novo array.
+    */
+
+  setNewCommentText('');
+  // Limpa o campo de input após adicionar o comentário.
+ }
+
+ return (
+  <article className={styles.post}>
+   <header className={styles.head}>
+    <div className={styles.author}>
+     <Avatar src={author.avatarUrl} />
+     <div className={styles.authorInfo}>
+      <strong>{author.name}</strong>
+      <span>{author.role}</span>
+     </div>
+    </div>
+    <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()} className={styles.data}>
+     {publishedDateRelativeNow}
+    </time>
+   </header>
+
+   <div className={styles.content}>
+    {content.map(line => {
+     if (line.type === 'paragraph') {
+      return <p key={line.content}>{line.content}</p>;
+     }
+     if (line.type === 'link') {
+      return (
+       <p key={line.content}>
+        <a href="#">{line.content}</a>
+       </p>
+      );
+     } else {
+      return <p>Erro ao retornar Valores</p>;
+     }
+    })}
+   </div>
+   <form onSubmit={handlleCreateNewComment} className={styles.commentForm}>
+    <strong>Deixe seu Feedback</strong>
+
+    <textarea name="textArea" placeholder="Deixe seu Feedback aqui" value={newCommentText} onChange={handlleCreateNewCommentChange} />
+
+    <footer>
+     <button type="submit">Comentar</button>
+    </footer>
+   </form>
+
+   <div className={styles.commentList}>
+    {comments.map(comment => {
+     return <Coment key={comment} content={comment} />;
+    })}
+   </div>
+  </article>
+ );
 }
